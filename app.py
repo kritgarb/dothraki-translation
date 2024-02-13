12c5de4ad0328312a73f5ec13afa6ee8bf5f92a5
Benjamin <garbkrit@gmail.com>
2024-02-13 15:56:42 -0300

    chore: enhance error handling in translation API communication
    - Improve exception handling with more specific exception types.
    - Check API response status code for better error differentiation.
    - Provide descriptive error messages for a more user-friendly experience.
    - Handle cases where the API response cannot be converted to JSON.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from requests.exceptions import ConnectionError, RequestException
 from flask_bootstrap import Bootstrap
 import requests
 
@@ -16,15 +17,25 @@ def translate_to_dothraki(text, api_key=None):
     
     try:
         response = requests.post(base_url, headers=headers, json=payload)
-        response.raise_for_status()  # Lança uma exceção para códigos de status diferentes de 2xx
-        result = response.json()
-        if 'contents' in result and 'translated' in result['contents']:
-            translated_text = result['contents']['translated']
-            return translated_text
+        
+        if response.status_code == 200:
+            try:
+                result = response.json()
+                if 'contents' in result and 'translated' in result['contents']:
+                    translated_text = result['contents']['translated']
+                    return translated_text
+                else:
+                    raise ValueError(f"Erro: Resposta inesperada da API - {result}")
+            except ValueError:
+                return "Erro: Resposta da API não pôde ser convertida para JSON."
         else:
-            return f"Erro: Resposta inesperada da API - {result}"
-    except requests.exceptions.RequestException as e:
-        return f"Erro na requisição: {e}"
+            return f"Erro na requisição: {response.status_code}, {response.text}"
+
+    except ConnectionError as ce:
+        return f"Erro na conexão: {ce}. Verifique sua conexão com a internet e tente novamente."
+
+    except RequestException as re:
+        return f"Erro na requisição: {re}. Verifique sua conexão com a internet e tente novamente."
 
 @app.route('/')
 def index():
